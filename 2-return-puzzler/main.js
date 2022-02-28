c3c1bc18f6a8fa95c14333b7c46ad89b39567db7
David Yarbrough <yarbroughdavidc@gmail.com>
2022-02-28 17:35:48 -0600

    adding a cleaned-up version of the end of the return puzzler

--- 2-return-puzzler/main.js
+++ 2-return-puzzler/main.js
@@ -1,9 +1,21 @@
+const urTaco = {
+    tortilla: true,
+    meat: true,
+    onions: true,
+    cilantro: {
+        amount: "normal"
+    },
+}
+
 const tacoFactory = () => {
-    const newTaco = {}
-    newTaco.tortilla = true
-    newTaco.meat = true
-    newTaco.onions = true
-    newTaco.cilantro = true
+    const newTaco = {...urTaco} // ??? what is the "..." operator doing here?
+    // const newTaco = {}
+    // newTaco.tortilla = true
+    // newTaco.meat = true
+    // newTaco.onions = true
+    // newTaco.cilantro = {
+    //     amount: "normal"
+    // }
 
     return newTaco
 }
@@ -12,8 +24,10 @@ const tacoOne = tacoFactory()
 const tacoTwo = tacoFactory()
 const tacoThree = tacoFactory()
 
-tacoThree.cilantro = false;
+// ??? what's the difference between these two lines?
+tacoThree.cilantro = {amount: "extra"};
+// tacoThree.cilantro.amount = "extra"
 
-console.log(`Taco One has cilantro: ${tacoOne.cilantro}`)
-console.log(`Taco Two has cilantro: ${tacoTwo.cilantro}`)
-console.log(`Taco Three has cilantro: ${tacoThree.cilantro}`)
+console.log(`Taco One has: ${JSON.stringify(tacoOne.cilantro.amount)} cilantro`)
+console.log(`Taco Two has: ${JSON.stringify(tacoTwo.cilantro.amount)} cilantro`)
+console.log(`Taco Three has: ${JSON.stringify(tacoThree.cilantro.amount)} cilantro`)
